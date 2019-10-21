0ef5be31bd8a775230a5ade352a8c5b903460855
Matthew Brisebois <matthew@webheroes.ca>
2019-10-21 14:13:21 -0600

    Add failure tests o iframe timeout o COMB handshake timeout o method timeout o undefined method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
+import Postmate				from 'postmate';
 
-import Postmate			from 'postmate';
+import { logging }			from '@holo-host/service-worker-logger';
+
+const log				= logging.getLogger('COMB');
+log.setLevel('debug');
+
+class TimeoutError extends Error {
+
+    timeout	: number;
+    
+    constructor( message : string, timeout : number, ...params ) {
+	// Pass remaining arguments (including vendor specific ones) to parent constructor
+	super( message );
+
+	// Maintains proper stack trace for where our error was thrown (only available on V8)
+	if ( Error.captureStackTrace ) {
+	    Error.captureStackTrace( this, TimeoutError );
+	}
+
+	this.name			= 'TimeoutError';
+	this.timeout			= timeout;
+    }
+}
+
+function async_with_timeout ( fn, timeout = 2000 ) : Promise<any> {
+    return new Promise(async (f,r) => {
+	log.debug("Set timeout async timeout to", timeout );
+	const to_id			= setTimeout(() => {
+	    log.warn("Triggered async timeout");
+	    r( new TimeoutError("Waited for " + (timeout/1000) + " seconds", timeout ) );
+	}, timeout);
+
+	try {
+	    const result		= await fn();
+	    f( result );
+	} catch ( err ) {
+	    r( err );
+	} finally {
+	    clearTimeout( to_id );
+	}
+    });
+}
+
 
 class ChildAPI {
 
@@ -12,25 +54,65 @@ class ChildAPI {
 
 	// if ( debug )
 	//     Postmate.debug	= true;
-	this.url		= url;
-	this.msg_count		= 0;
-	this.responses		= {};
+	this.url			= url;
+	this.msg_count			= 0;
+	this.responses			= {};
     }
 
     async connect () {
-	const chaperon		= await new Postmate({
-	    "container": document.body,
-	    "url": this.url,
-	    "classListArray": ["chaperon-frame"],
-	});
+	let loaded			= false;
+	let chaperon;
+	
+	try {
+	    chaperon			= await async_with_timeout(async () => {
+		log.info("Init Postmate handshake");
+		const handshake		= new Postmate({
+		    "container": document.body,
+		    "url": this.url,
+		    "classListArray": ["chaperon-frame"],
+		});
 
+		const iframe		= document.querySelector('iframe.chaperon-frame');
+		log.debug("Listening for iFrame load event", iframe );
+		
+		iframe['contentWindow'].addEventListener("domcontentloaded", () => {
+		    log.debug("iFrame content has loaded");
+		    loaded		= true;
+		});
+
+		return await handshake;
+	    }, 1000 );
+	} catch ( err ) {
+	    if ( err.name === "TimeoutError" ) {
+		if ( loaded ) {
+		    log.error("iFrame loaded but could not communicate with COMB");
+		    throw new TimeoutError("Failed to complete COMB handshake", err.timeout );
+		}
+		else {
+		    log.error("iFrame did not trigger load event");
+		    throw new TimeoutError("Failed to load iFrame", err.timeout );
+		}
+	    }
+	    else
+		throw err;
+	}
+	
+	log.info("Finished handshake");
+	
 	chaperon.on('response', ( data ) => {
-	    let [k,v]		= data;
-	    this.responses[ k ]( v );
+	    let [k,v]			= data;
+	    log.info("Received response for msg_id:", k );
+
+	    const [f,r]			= this.responses[ k ];
+
+	    if ( v instanceof Error )
+		r( v );
+	    else
+		f( v );
 	});
 
-	this.msg_bus		= chaperon;
-
+	this.msg_bus			= chaperon;
+	
 	return this;
     }
 
@@ -39,15 +121,18 @@ class ChildAPI {
     }
 
     run ( method, data ) {
-	let msg_id		= this.msg_count++;
+	let msg_id			= this.msg_count++;
 	
 	this.msg_bus.call( "exec", [ msg_id, method, data ] );
+	log.info("Sent request with msg_id:", msg_id );
 	
-	return new Promise((f,r) => {
-	    this.responses[msg_id]	= f;
-
-	    // TODO: make a timout that calls r
-	});
+	return async_with_timeout(async () => {
+	    const request		= new Promise((f,r) => {
+		this.responses[msg_id]	= [f,r];
+	    });
+	    
+	    return await request;
+	}, 1000 );
     }
 }
 
@@ -71,7 +156,14 @@ class ParentAPI {
 	    "exec": async ( data ) => {
 		const [msg_id, method, ...args] = data;
 
-		const resp		= await this.methods[ method ].apply( this, args );
+		const fn		= this.methods[ method ];
+		
+		if ( fn === undefined ) {
+		    log.error("Method does not exist", method );
+		    return parent.emit("response", [ msg_id, new Error("Method '"+ method +"' does not exist") ]);
+		}
+		
+		const resp		= await fn.apply( this, args );
 		
 		parent.emit("response", [ msg_id, resp ]);
 	    }
--- tests/integration/test_comb.js
+++ tests/integration/test_comb.js
@@ -12,37 +12,49 @@ const puppeteer				= require('puppeteer');
 const http_servers			= require('../setup.js');
 
 
+let browser;
+
+async function create_page ( url ) {
+    const page				= await browser.newPage();
+    
+    page.on("console", async ( msg ) => {
+    	log.silly("From puppeteer: console.log( %s )", msg.text() );
+    });
+    
+    log.info("Go to: %s", url );
+    await page.goto( url, { "waitUntil": "networkidle0" } );
+
+    return page;
+}
+
+
 describe("Testing COMB", function() {
 
-    let setup, browser;
+    let setup, happ_host, chap_host;
 	
-    before("", async () => {
+    before("Start servers and browser", async () => {
 	setup				= http_servers();
 	browser				= await puppeteer.launch();
 	
 	log.debug("Setup config: %s", setup.ports );
+
+    	happ_host			= `http://localhost:${setup.ports.happ}`;
+    	chap_host			= `http://localhost:${setup.ports.chaperon}`;
     });
 
-    after("", async () => {
+    after("Close servers and browser", async () => {
 	log.debug("Shutdown cleanly...");
 	await browser.close();
 	await setup.close();
     });
 	
-    it('should insert Chaperon iframe into hApp window', async function () {
+    it("should insert Chaperon iframe into hApp window", async function () {
+	const happ_url			= `${happ_host}/index.html`
+	const chap_url			= `${chap_host}/index.html`
+
+    	const page			= await create_page( happ_url );
+
     	try {
-    	    const happ_url		= `http://localhost:${setup.ports.happ}/index.html`;
-    	    const chap_url		= `http://localhost:${setup.ports.chaperon}/index.html`;
-	    
-    	    const page			= await browser.newPage();
-
-    	    page.on("console", async ( msg ) => {
-    		log.debug("From puppeteer: console.log( %s )", msg.text() );
-    	    });
-	    
-    	    log.info("Go to: %s", happ_url );
-    	    await page.goto( happ_url, { "waitUntil": "networkidle0" } );
-
 	    await page.evaluate(async function ( frame_url )  {
 		const child		= await COMB.connect( frame_url );
 	    }, chap_url );
@@ -57,23 +69,17 @@ describe("Testing COMB", function() {
 
     	    expect( frames[0].url()	).to.equal( chap_url );
     	} finally {
+	    await page.close();
     	}
     });
 	
-    it('should call method on child and await response', async function () {
+    it("should call method on child and await response", async function () {
+	const happ_url			= `${happ_host}/index.html`
+	const chap_url			= `${chap_host}/index.html`
+
+    	const page			= await create_page( happ_url );
+
 	try {
-	    const happ_url		= `http://localhost:${setup.ports.happ}/index.html`;
-	    const chap_url		= `http://localhost:${setup.ports.chaperon}/index.html`;
-	    
-	    const page			= await browser.newPage();
-
-	    page.on("console", async ( msg ) => {
-		log.debug("From puppeteer: console.log( %s )", msg.text() );
-	    });
-	    
-	    log.info("Go to: %s", happ_url );
-	    await page.goto( happ_url, { "waitUntil": "networkidle0" } );
-
 	    const answer		= await page.evaluate(async function ( frame_url )  {
 		const child		= await COMB.connect( frame_url );
 		return await child.run("test");
@@ -81,7 +87,114 @@ describe("Testing COMB", function() {
 
 	    expect( answer	).to.equal( "Hello World" );
 	} finally {
+	    await page.close();
 	}
     });
-    
+	
+    it("should timeout because of wrong frame URL", async function () {
+	const happ_url			= `${happ_host}/index.html`
+
+    	const page			= await create_page( happ_url );
+
+	try {
+	    const result		= await page.evaluate(async function ()  {
+		try {
+		    await COMB.connect( "http://localhost:55555" );
+		} catch ( err ) {
+		    console.log( "Error message value:", err.message );
+		    return {
+			"name": err.name,
+			"message": err.message,
+		    };
+		}
+	    });
+	    log.debug("Error result: %s", result );
+
+	    expect( result.name		).to.equal( "TimeoutError" );
+	    expect( result.message	).to.equal( "Failed to load iFrame" );
+	} finally {
+	    await page.close();
+	}
+    });
+	
+    it("should timeout because COMB is not listening", async function () {
+	const happ_url			= `${happ_host}/index.html`
+	const fail_url			= `${chap_host}/comb_not_listening.html`
+
+    	const page			= await create_page( happ_url );
+
+	try {
+	    const result		= await page.evaluate(async function ( frame_url )  {
+		try {
+		    await COMB.connect( frame_url );
+		} catch ( err ) {
+		    console.log( "Error message value:", err.message );
+		    return {
+			"name": err.name,
+			"message": err.message,
+		    };
+		}
+	    }, fail_url );
+
+	    expect( result.name		).to.equal( "TimeoutError" );
+	    expect( result.message	).to.equal( "Failed to load iFrame" );
+	} finally {
+	    await page.close();
+	}
+    });
+	
+    it("should timeout because method didn't respond", async function () {
+	const happ_url			= `${happ_host}/index.html`
+	const fail_url			= `${chap_host}/comb_method_no_response.html`
+
+    	const page			= await create_page( happ_url );
+
+	try {
+	    const result		= await page.evaluate(async function ( frame_url )  {
+		try {
+		    const child		= await COMB.connect( frame_url );
+		    await child.run("timeout");
+		} catch ( err ) {
+		    console.log( "Error message value:", err.message );
+		    return {
+			"name": err.name,
+			"message": err.message,
+		    };
+		}
+	    }, fail_url );
+
+	    expect( result.name		).to.equal( "TimeoutError" );
+	    expect( result.message	).to.equal( "Waited for 1 seconds" );
+	} finally {
+	    await page.close();
+	}
+    });
+	
+    it("should throw error because method does not exist", async function () {
+	const happ_url			= `${happ_host}/index.html`
+	const fail_url			= `${chap_host}/comb_method_does_not_exist.html`
+
+    	const page			= await create_page( happ_url );
+
+	try {
+	    const result		= await page.evaluate(async function ( frame_url )  {
+		try {
+		    const child		= await COMB.connect( frame_url );
+		    await child.run("undefined_method");
+		} catch ( err ) {
+		    console.log( "Error message value:", err.message );
+		    return {
+			"name": err.name,
+			"message": err.message,
+		    };
+		}
+	    }, fail_url );
+
+	    expect( result.name		).to.equal( "Error" );
+	    expect( result.message	).to.equal( "Method 'undefined_method' does not exist" );
+	} finally {
+	    await page.close();
+	}
+    });
+	
 });
